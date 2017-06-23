4f6c35ab54933acfb9dea9dcd489531e3ed04682
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2017-06-23 15:39:43 +0100

    copy to M4

---
+++ M4/ECommerce.ProductCatalog.Model/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ECommerce.ProductCatalog.Model
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public int Availability { get; set; }
+    }
+}
---
+++ M4/ECommerce.ProductCatalog/ProductCatalog.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Data.Collections;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using ECommerce.ProductCatalog.Model;
+using Microsoft.ServiceFabric.Services.Remoting.Runtime;
+
+namespace ECommerce.ProductCatalog
+{
+    /// <summary>
+    /// An instance of this class is created for each service replica by the Service Fabric runtime.
+    /// </summary>
+    internal sealed class ProductCatalog : StatefulService, IProductCatalogService
+    {
+        private IProductRepository _repo;
+
+        public ProductCatalog(StatefulServiceContext context)
+            : base(context)
+        { }
+
+        public async Task AddProduct(Product product)
+        {
+            await _repo.AddProduct(product);
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProducts()
+        {
+            return await _repo.GetAllProducts();
+        }
+
+        /// <summary>
+        /// Optional override to create listeners (e.g., HTTP, Service Remoting, WCF, etc.) for this service replica to handle client or user requests.
+        /// </summary>
+        /// <remarks>
+        /// For more information on service communication, see https://aka.ms/servicefabricservicecommunication
+        /// </remarks>
+        /// <returns>A collection of listeners.</returns>
+        protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
+        {
+            return new[]
+            {
+                new ServiceReplicaListener(context => this.CreateServiceRemotingListener(context))
+            };
+
+        }
+
+        /// <summary>
+        /// This is the main entry point for your service replica.
+        /// This method executes when this replica of your service becomes primary and has write status.
+        /// </summary>
+        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service replica.</param>
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            _repo = new ServiceFabricProductRepository(this.StateManager);
+
+            var product1 = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = "Dell Monitor",
+                Description = "Computer Monitor",
+                Price = 500,
+                Availability = 100
+            };
+
+            var product2 = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = "Surface Book",
+                Description = "Microsoft's Latest Laptop, i7 CPU, 1Tb SSD",
+                Price = 2200,
+                Availability = 15
+            };
+
+            var product3 = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = "Arc Touch Mouse",
+                Description = "Computer Mouse, bluetooth, requires 2 AAA batteries",
+                Price = 60,
+                Availability = 30
+            };
+
+            await _repo.AddProduct(product1);
+            await _repo.AddProduct(product2);
+            await _repo.AddProduct(product3);
+
+            IEnumerable<Product> all = await _repo.GetAllProducts();
+        }
+    }
+}
