e5855c794ca10c794d1dea18b538786b62d5eebf
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2017-08-09 12:22:36 +0100

    add http controller

---
+++ M4/ECommerce.API/Controllers/BasketController.cs
@@ -0,0 +1,31 @@
+﻿using ECommerce.API.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class BasketController : Controller
+    {
+        [HttpGet("{userId}")]
+        public async Task<ApiBasket> Get(string userId)
+        {
+            return new ApiBasket() {UserId = userId.ToString()};
+        }
+
+        [HttpPost("{userId}")]
+        public async Task Add([FromBody] ApiBasketAddRequest request)
+        {
+            int i = 0;
+        }
+
+        [HttpDelete("{userId}")]
+        public async Task Delete(int userId)
+        {
+            int i = 0;
+        }
+    }
+}
