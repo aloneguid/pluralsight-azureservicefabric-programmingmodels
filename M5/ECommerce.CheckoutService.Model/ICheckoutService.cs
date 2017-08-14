a5a456bec25d8db3b1b7ee019f9e324f4faa7206
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2017-08-14 18:14:06 +0100

    - add CheckoutProduct - add CheckoutSummary - add remote interface ICheckoutService

--- M5/ECommerce.CheckoutService.Model/ICheckoutService.cs
+++ M5/ECommerce.CheckoutService.Model/ICheckoutService.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.ServiceFabric.Services.Remoting;
 
 namespace ECommerce.CheckoutService.Model
 {
-    public interface ICheckoutService : IService
-    {
-    }
+   public interface ICheckoutService : IService
+   {
+      Task<CheckoutSummary> Checkout(string userId);
+
+      Task<IEnumerable<CheckoutSummary>> GetOrderHitory(string userId);
+   }
 }
