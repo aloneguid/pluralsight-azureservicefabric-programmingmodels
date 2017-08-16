d845cb97a0bdf18ba47882409dadf73bbd3a9b3c
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2017-08-16 23:16:27 +0100

    checkout service: - add references to: Microsoft.ServiceFabric.Actors, UserActor.Interface, ProductCatalog.Model - add helper methods GetUserActor, GetProductCatalogService - call to get basket

--- M5/ECommerce.CheckoutService/CheckoutService.cs
+++ M5/ECommerce.CheckoutService/CheckoutService.cs
@@ -9,6 +9,12 @@ using Microsoft.ServiceFabric.Services.Communication.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
 using Microsoft.ServiceFabric.Services.Remoting.Runtime;
 using ECommerce.CheckoutService.Model;
+using UserActor.Interfaces;
+using Microsoft.ServiceFabric.Actors.Client;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using ECommerce.ProductCatalog.Model;
+using Microsoft.ServiceFabric.Services.Client;
 
 namespace ECommerce.CheckoutService
 {
@@ -21,8 +27,15 @@ namespace ECommerce.CheckoutService
           : base(context)
       { }
 
-      public Task<CheckoutSummary> Checkout(string userId)
+      public async Task<CheckoutSummary> Checkout(string userId)
       {
+         //call user actor to get the basket
+         IUserActor userActor = GetUserActor(userId);
+         Dictionary<Guid, int> basket = await userActor.GetBasket();
+
+         //get catalog client
+         IProductCatalogService catalogService = GetProductCatalogService();
+
          throw new NotImplementedException();
       }
 
@@ -30,6 +43,19 @@ namespace ECommerce.CheckoutService
       {
          throw new NotImplementedException();
       }
+
+      private IUserActor GetUserActor(string userId)
+      {
+         return ActorProxy.Create<IUserActor>(new ActorId(userId), new Uri("fabric:/ECommerce/UserActorService"));
+      }
+
+      private IProductCatalogService GetProductCatalogService()
+      {
+         return ServiceProxy.Create<IProductCatalogService>(
+            new Uri("fabric:/ECommerce/ProductCatalog"),
+            new ServicePartitionKey(0));
+      }
+
 
       protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
       {
