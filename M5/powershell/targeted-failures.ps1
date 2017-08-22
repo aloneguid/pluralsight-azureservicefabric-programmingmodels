d9bca56dcebd46e6e84539c1be6bf318ced6938c
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2017-08-22 16:58:08 +0100

    script to register application, create an instance of it, and kill everything back

--- M5/powershell/targeted-failures.ps1
+++
@@ -1,6 +0,0 @@
-Connect-ServiceFabricCluster
-
-$nodes = Get-ServiceFabricNode
-
-# https://docs.microsoft.com/en-us/powershell/module/servicefabric/start-servicefabricpartitionrestart?view=azureservicefabricps
-
