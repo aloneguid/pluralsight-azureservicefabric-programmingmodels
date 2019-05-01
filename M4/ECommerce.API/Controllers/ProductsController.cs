1cbe63bc29c0f57d4b434988afe01a76295aa519
Ivan G <ivan@isolineltd.com>
2019-05-01 22:35:54 +0100

    m4-06: adding web api with fake data returns

---
+++ M4/ECommerce.API/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerce.API.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.API.Controllers
+{
+   [Route("api/[controller]")]
+   [ApiController]
+   public class ProductsController : ControllerBase
+   {
+      [HttpGet]
+      public async Task<IEnumerable<ApiProduct>> GetAsync()
+      {
+         return new[] { new ApiProduct { Id = Guid.NewGuid(), Description = "fake" } };
+      }
+
+      [HttpPost]
+      public async Task PostAsync([FromBody] ApiProduct product)
+      {
+      }
+   }
+}
