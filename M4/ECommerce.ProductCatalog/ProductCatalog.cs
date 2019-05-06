82c147d1786320f66c518b9f62d6ee5fd53bfda7
Ivan G <ivan@isolineltd.com>
2019-05-06 16:55:22 +0100

    Revert "m04-07 - remoting fix"
    This reverts commit 6376b0676cba4569a734520f5502d0a3e77aea21.

--- M4/ECommerce.ProductCatalog/ProductCatalog.cs
+++ M4/ECommerce.ProductCatalog/ProductCatalog.cs
@@ -8,7 +8,6 @@ using ECommerce.ProductCatalog.Model;
 using Microsoft.ServiceFabric.Data;
 using Microsoft.ServiceFabric.Data.Collections;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
-using Microsoft.ServiceFabric.Services.Remoting.V2.FabricTransport.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
 
 namespace ECommerce.ProductCatalog
@@ -16,23 +15,13 @@ namespace ECommerce.ProductCatalog
    /// <summary>
    /// An instance of this class is created for each service replica by the Service Fabric runtime.
    /// </summary>
-   internal sealed class ProductCatalog : StatefulService, IProductCatalogService
+   internal sealed class ProductCatalog : StatefulService
    {
       private IProductRepository _repo;
 
       public ProductCatalog(StatefulServiceContext context)
           : base(context)
       { }
-
-      public async Task AddProductAsync(Product product)
-      {
-         await _repo.AddProduct(product);
-      }
-
-      public async Task<Product[]> GetAllProductsAsync()
-      {
-         return (await _repo.GetAllProducts()).ToArray();
-      }
 
       /// <summary>
       /// Optional override to create listeners (e.g., HTTP, Service Remoting, WCF, etc.) for this service replica to handle client or user requests.
@@ -41,14 +30,9 @@ namespace ECommerce.ProductCatalog
       /// For more information on service communication, see https://aka.ms/servicefabricservicecommunication
       /// </remarks>
       /// <returns>A collection of listeners.</returns>
-      protected override IEnumerable<ServiceReplicaListener> 
-         CreateServiceReplicaListeners()
+      protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
       {
-         return new[]
-         {
-            new ServiceReplicaListener(context => 
-               new FabricTransportServiceRemotingListener(context, this))
-         };
+         return new ServiceReplicaListener[0];
       }
 
       /// <summary>
