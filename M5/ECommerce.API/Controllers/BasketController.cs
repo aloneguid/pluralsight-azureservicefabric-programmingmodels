0abc0f19668d3fc2bf19f656c0877c0e32923231
Ivan G <ivan@isolineltd.com>
2019-05-14 21:48:43 +0100

    05-05 basket controller with empty methods

---
+++ M5/ECommerce.API/Controllers/BasketController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerce.API.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.API.Controllers
+{
+   [Route("api/[controller]")]
+   [ApiController]
+   public class BasketController : ControllerBase
+   {
+      [HttpGet("{userId}")]
+      public async Task<ApiBasket> GetAsync(string userId)
+      {
+
+      }
+
+      [HttpPost("{userId}")]
+      public async Task AddAsync(
+         string userId,
+         [FromBody] ApiBasketAddRequest request)
+      {
+
+      }
+
+      [HttpDelete("{userId}")]
+      public async Task DeleteAsync(string userId)
+      {
+
+      }
+   }
+}
